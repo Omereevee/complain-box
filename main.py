11113c2e856e302f38aba38715f8f82c9975e1c4
Your Name <irfana.27@gmail.com>
2026-07-13 20:17:48 -0700

    second commit

--- main.py
+++ main.py
@@ -12,8 +12,8 @@ chrome_options.add_experimental_option("detach", True)
 driver = webdriver.Chrome(options=chrome_options)
 
 # ---- constants ---- #
-PROMISED_UP = None
-PROMISED_DOWN = None
+PROMISED_UP = 50
+PROMISED_DOWN = 256
 
 Y_EMAIL = "omerkumble@gmail.com"
 Y_PASSWORD = "jgpj9wgh1t2nY0Yt"
@@ -22,10 +22,10 @@ SPEED_URL = "https://speedtest.net"
 
 # ---- logic ---- #
 class InternetSpeedYBot:
-    def __init__(self, up, down):
+    def __init__(self):
         self.driver = driver
-        self.up = up
-        self.down = down
+        self.up = None
+        self.down = None
 
     def get_internet_speed(self):
         self.driver.get(SPEED_URL)
@@ -46,13 +46,31 @@ class InternetSpeedYBot:
         print(self.up)
         print(self.down)
 
+        if int(self.up) > PROMISED_UP or  int(self.down) > PROMISED_DOWN:
+            self.tweet_at_provider()
+
     def tweet_at_provider(self):
-        pass
+        self.driver.get(Y_LOGIN_URL)
+
+        self.y_email = self.driver.find_element(By.ID,"email")
+        self.y_password = self.driver.find_element(By.ID,"password")
+        self.y_login_button = self.driver.find_element(By.CLASS_NAME,"y-login-submit")
+
+        self.y_email.send_keys(Y_EMAIL)
+        self.y_password.send_keys(Y_PASSWORD)
+        self.y_login_button.click()
+
+        self.y_tweet = self.driver.find_element(By.CLASS_NAME,"x-compose")
+        self.y_send = self.driver.find_element(By.ID,"post-btn")
+
+        self.y_tweet.send_keys(f"why is my internet speed {self.up} up/{self.down} down when i pay for {PROMISED_UP} up/{PROMISED_DOWN} down")
+        self.y_send.click()
+
 
 # ---- Execution ---- #
-InternetSpeedYBot = InternetSpeedYBot(100, 10)
+
+InternetSpeedYBot = InternetSpeedYBot()
 
 InternetSpeedYBot.get_internet_speed()
-InternetSpeedYBot.tweet_at_provider()
 
 
